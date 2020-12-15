e68e03b5f330e5cea7e674a20d3b540d85edfb8e
Giacomo <giacomo.1936@gmail.com>
2020-12-15 16:59:40 +0100

    test echo nomi squadre in casa

--- php-snack-1/index.php
+++ php-snack-1/index.php
@@ -1,29 +1,46 @@
 <?php
-    $matches [
-        "match" => [
-            "squadra casa" => "Olimpia Milano",
-            "squadra ospite" => "Fortitudo",
-            "punti-squadra-casa" => 75,
-            "punti-squadra-ospite" => 80
+    $matches = [
+        [
+            'squadra-casa' => 'Olimpia Milano',
+            'squadra-ospite' => 'Fortitudo',
+            'punti-squadra-casa' => 103,
+            'punti-squadra-ospite' => 98
         ],
-        "match" => [
-            "squadra casa" => "Trento",
-            "squadra ospite" => "Teramo",
-            "punti-squadra-casa" => 70,
-            "punti-squadra-ospite" => 62
+        [
+            'squadra-casa' => 'Padova',
+            'squadra-ospite' => 'Virtus Bologna',
+            'punti-squadra-casa' => 70,
+            'punti-squadra-ospite' => 90
         ],
-        "match" => [
-            "squadra casa" => "Virtus Bologna",
-            "squadra ospite" => "Roma",
-            "punti-squadra-casa" => 80,
-            "punti-squadra-ospite" => 93
+        [
+            'squadra-casa' => 'Teramo',
+            'squadra-ospite' => 'Rieti',
+            'punti-squadra-casa' => 65,
+            'punti-squadra-ospite' => 60
         ],
-        "match" => [
-            "squadra casa" => "Rieti",
-            "squadra ospite" => "Padova",
-            "punti-squadra-casa" => 62,
-            "punti-squadra-ospite" => 60
-        ],
+        [
+            'squadra-casa' => 'Trento',
+            'squadra-ospite' => 'Bari',
+            'punti-squadra-casa' => 95,
+            'punti-squadra-ospite' => 85
+        ]
     ];
 
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+     <head>
+         <meta charset="utf-8">
+         <title></title>
+     </head>
+     <body>
+         <div class="">
+             <?php
+                 for($i = 0; $i < count($matches); $i++) {
+                     echo $matches[$i]['squadra-casa'];
+                 }
+              ?>
+         </div>
+     </body>
+ </html>
