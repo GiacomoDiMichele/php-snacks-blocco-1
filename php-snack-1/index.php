cc5934a463439f7b055e2a61cdeedf7482a9ee2b
Giacomo <giacomo.1936@gmail.com>
2020-12-15 16:06:45 +0100

    test snack 1

---
+++ php-snack-1/index.php
@@ -0,0 +1,29 @@
+<?php
+    $matches [
+        "match" => [
+            "squadra casa" => "Olimpia Milano",
+            "squadra ospite" => "Fortitudo",
+            "punti-squadra-casa" => 75,
+            "punti-squadra-ospite" => 80
+        ],
+        "match" => [
+            "squadra casa" => "Trento",
+            "squadra ospite" => "Teramo",
+            "punti-squadra-casa" => 70,
+            "punti-squadra-ospite" => 62
+        ],
+        "match" => [
+            "squadra casa" => "Virtus Bologna",
+            "squadra ospite" => "Roma",
+            "punti-squadra-casa" => 80,
+            "punti-squadra-ospite" => 93
+        ],
+        "match" => [
+            "squadra casa" => "Rieti",
+            "squadra ospite" => "Padova",
+            "punti-squadra-casa" => 62,
+            "punti-squadra-ospite" => 60
+        ],
+    ];
+
+ ?>
